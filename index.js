8b352f2e8a14042885ce1e0916e263657d4f1940
Danila Kuzmuk <Da.Kuzmuk@ssu.edu.ua>
2021-01-23 23:32:11 +0200

    Optimization

--- index.js
+++ index.js
@@ -1,72 +1,56 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, { wsEngine: 'ws' });
-
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, { wsEngine: "ws" });
 
 let users = [];
-
+let id = 0;
 let start = {
-    "p_x": 0.0,
-    "p_y": 0.0,
-    "p_z": 0.0,
-    "r_x": 0.0,
-    "r_y": 0.0,
-    "r_z": 0.0,
-    "r_w": 0.0,
-}
+  p_x: 0.0,
+  p_y: 0.0,
+  p_z: 0.0,
+  r_x: 0.0,
+  r_y: 0.0,
+  r_z: 0.0,
+  r_w: 0.0,
+};
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', async(socket) => {
-    socket.emit("connect")
-    socket.on("join", async()=>{
-        console.log("join")
-        io.sockets.sockets;
-       // console.log(users);
-       start["id"] = socket.id;
-        socket.broadcast.emit("enterNewPlayer",start);
-        socket.broadcast.emit("getUserInfo",{id:socket.id})
-         users[socket.id] =await socket.id;
-    })
+io.on("connection", async (socket) => {
+  socket.emit("connect");
+  socket.on("join", async () => {
+    console.log("join");
+    socket.emit("setObjId", { id: id });
+    start["id"] = id;
+    socket.broadcast.emit("enterNewPlayer", start);
+    socket.broadcast.emit("getUserInfo", { id: socket.id });
+    users[socket.id] = await socket.id;
+    id++;
+  });
 
-    socket.on("disconnect", async()=>{
-        console.log("disconnect");
-        socket.broadcast.emit("playerLeave", {id:socket.id});
-        delete(users[socket.id]);
-    })
+  socket.on("disconnect", async () => {
+    console.log("disconnect");
+    socket.broadcast.emit("playerLeave", { id: socket.id });
+    delete users[socket.id];
+    if (users.length === 0) {
+      id = 0;
+    }
+  });
 
-    socket.on("sending-event", async (jsonObj) => {
-        socket.broadcast.to(jsonObj["room_id"]).emit(jsonObj["resiver"], jsonObj);
-      });
+  socket.on("sending-event", async (jsonObj) => {
+    socket.broadcast.to(jsonObj["room_id"]).emit(jsonObj["resiver"], jsonObj);
+  });
 
-      socket.on("sending-event-all", async (jsonObj) => {
-          //console.log(jsonObj);
-        socket.broadcast.emit(jsonObj["resiver"], jsonObj);
-      });
-
+  socket.on("sending-event-all", async (jsonObj) => {
+    let resiever = jsonObj["resiver"];
+    delete jsonObj["resiver"];
+    console.log(jsonObj);
+    socket.broadcast.emit(resiever, jsonObj);
+  });
 });
 
-http.listen(process.env.PORT|| 3000, () => {
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, () => {
+  console.log("listening on *:3000");
 });
-
-server.on('error', (err) => {
-    console.log(`server error:\n${err.stack}`);
-    server.close();
-  });
-  
-  server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-  });
-  
-  server.on('listening', () => {
-    var address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
-  });
-  
-  server.bind(process.env.PORT|| 3000);
-  // server listening 0.0.0.0:41234
